6a0b16f779440e171490f42063dc691eae883faf
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-24 21:24:29 -0800

    Add jwt token - private route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const keys = require('../../config/keys');
 const User = require('../../models/User');
@@ -87,5 +88,20 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route         GET api/users/current
+// @description   Return current user
+// @access        Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
