72d2b4191f986429a18bcfbe0f13fe3b4d6db51f
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-26 14:15:23 -0800

    Add POST /posts - DELETE /posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -264,4 +264,19 @@ router.delete(
   }
 );
 
+// @route         DELETE api/profile
+// @description   DELETE Profile
+// @access        Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
 module.exports = router;
