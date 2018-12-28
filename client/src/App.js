127aae8bc4fcb3f94ba9f49720c04a22cf554a59
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-27 16:09:30 -0800

    Add redux login/logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import { Provider } from 'react-redux';
 
 import './App.css';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import store from './store';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth Token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and experation
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route path="/" exact component={Landing} />
-          <div className="container">
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Route path="/" exact component={Landing} />
+            <div className="container">
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
