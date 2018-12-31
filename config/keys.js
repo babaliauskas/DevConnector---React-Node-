0b42e580f7bb5c075e5b15719a49b29d85fc8624
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-12-31 13:12:48 -0800

    prepare for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb://babaliauskas:atsipalaidaves10@ds157459.mlab.com:57459/lukas-dev-connector',
-  secretOrKey: 'secretKey'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
